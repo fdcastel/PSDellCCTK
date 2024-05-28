0db620c0727535bea00f113a048b69f15f21a6a3
F.D.Castel <fdcastel@gmail.com>
2024-05-28 05:59:05 -0300

    Adds bootstrap.ps1 and README.md.

---
+++ README.md
@@ -0,0 +1,13 @@
+# PSDellCCTK
+
+
+
+## Installation
+
+To download all scripts into your `$env:temp` folder:
+
+```powershell
+iex (iwr 'https://raw.githubusercontent.com/fdcastel/PSDellCCTK/master/bootstrap.ps1' -UseBasicParsing)
+```
+
+> All scripts requires administrative privileges to run.
---
+++ bootstrap.ps1
@@ -0,0 +1,11 @@
+# Enables TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
+$url = 'https://codeload.github.com/fdcastel/PSDellCCTK/zip/master'
+$fileName = Join-Path $env:TEMP 'PSDellCCTK-master.zip'
+
+$ProgressPreference = 'SilentlyContinue'    # Faster downloads
+Invoke-RestMethod $url -OutFile $fileName
+Expand-Archive $fileName -DestinationPath $env:TEMP -Force
+
+Set-Location "$env:TEMP\PSDellCCTK-master"
